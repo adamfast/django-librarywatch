684689f5c63868aaad7d62c0d5f6072d034f0255
Adam Fast <adam@adamfast.com>
2012-09-16 11:51:49 -0500

    Add the ability to ignore packages or specific versions through a setting.

--- librarywatch/management/commands/check_for_updates.py
+++ librarywatch/management/commands/check_for_updates.py
@@ -1,6 +1,7 @@
 import pip
 import xmlrpclib
 
+from django.conf import settings
 from django.core.management.base import BaseCommand, CommandError
 
 '''
@@ -16,15 +17,19 @@ originally based off of:
 class Command(BaseCommand):
     def handle(self, *args, **options):
         pypi = xmlrpclib.ServerProxy('http://pypi.python.org/pypi')
+
+        ignores = getattr(settings, 'LIBRARYWATCH_IGNORE_VERSIONS', {})
+
         for dist in pip.get_installed_distributions():
-            available = pypi.package_releases(dist.project_name)
-            if not available:
-                # Try to capitalize pkg name
-                available = pypi.package_releases(dist.project_name.capitalize())
+            if ignores.get(dist.project_name, '') != '*':  # if it's totally ignored, don't even check.
+                available = pypi.package_releases(dist.project_name)
+                if not available:
+                    # Try to capitalize pkg name
+                    available = pypi.package_releases(dist.project_name.capitalize())
 
-            try:
-                if available[0] != dist.version:
-                    print '{dist.project_name} ({dist.version} != {available})'.format(dist=dist, available=available[0])
+                try:
+                    if available[0] != dist.version and available[0] != ignores.get(dist.project_name, ''):
+                        print '{dist.project_name} ({dist.version} != {available})'.format(dist=dist, available=available[0])
 
-            except IndexError:
-                pass
+                except IndexError:
+                    print('%s is not available on PyPI.' % dist.project_name)
